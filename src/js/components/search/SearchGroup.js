0ab0044aae46d39c75ed61076094c869da8ae60d
Dylan Cunningham <gdylanc@gmail.com>
2017-11-22 09:53:49 -0700

    search query in header produces a text link to go to a users page. it disappears and reappears based upon state of the search form

--- src/js/components/search/SearchGroup.js
+++ src/js/components/search/SearchGroup.js
@@ -3,22 +3,48 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { query } from '../../actions/query';
 import SearchResult from './SearchResult';
+import axios from 'axios';
 
 
 class SearchGroup extends Component {
   constructor() {
     super();
     this.state = {
-      query: null
+      query: null,
+      theQuery: null,
+      updated: false,
+      wasClicked: false
     }
   }
+
+  wasClicked = () => {
+    this.setState({
+      wasClicked: true
+    })
+  }
+
+
+  getQuery = (query) =>{
+    axios.get(`http://localhost:1337/users/findUserByUsername/${query}`)
+    .then(res => {
+      const theQuery = res.data;
+      this.setState({
+        theQuery: theQuery,
+        updated: true,
+        wasClicked: false
+      });
+    });
+  }
+
+
+
   render () {
 
     console.log('query', this.props.query);
     return (
       <div>
         <form
-          onSubmit={e => { this.props.queryAction(
+          onSubmit={e => { this.getQuery(
             this.state.query
           )
           e.preventDefault();
@@ -28,14 +54,15 @@ class SearchGroup extends Component {
         >
         <div className="formGroup">
           <div className="formComponent1">
-            <input className="form-control" name="query" placeholder="query" onChange={e => this.setState({query: e.target.value})}/>
+            <input className="form-control" name="query" placeholder="search username" onChange={e => this.setState({query: e.target.value})}/>
           </div>
           <div className="formComponent2">
             <input type="submit" className="btn btn-primary" />
           </div>
         </div>
         </form>
-        <SearchResult query={this.props.query}/>
+        {this.state.updated ? <SearchResult theQuery={this.state.theQuery} wasClickedState={this.state.wasClicked} wasClickedFunc = {this.wasClicked}/> : ''}
+
       </div>
     )
   }
